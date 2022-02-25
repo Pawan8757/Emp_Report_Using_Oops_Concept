fe1cefa0fd39a3f112b0e5d01eb6af0f53cb93e0
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-25 22:27:47 +0530

    Check whether employee is present or absent

---
+++ Prblm_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Report_Using_Oops
+{
+    public class EmplyAbsenty
+    {
+        public void CheckingAbsenty()
+        {
+            int number;
+            Random random = new Random();   
+            number = random.Next(2);
+
+            if(number == 0)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+        }
+    }
+}
