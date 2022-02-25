725b6147c511fe18b6de81c400849b2103da4de2
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-25 22:35:16 +0530

    monthly wage of a employee

---
+++ Prblm_5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Report_Using_Oops
+{
+    public class MonthlySalary
+    {
+        public void CalculatingMonthlyWage()
+        {
+            const int PartTime = 0;
+            const int FullTime = 1;
+            const int EmpPerHour = 20;
+            int wage = 0;
+            int EmpHrs;
+            int TotalWorkingDays = 20;
+            int TotalSalary = 0;
+
+
+
+            for (int i = 0; i <= TotalWorkingDays; i++)
+            {
+                Random random = new Random();
+                int number = random.Next(3);
+            
+                if (number == PartTime)
+                {
+                     EmpHrs = 4;
+
+                }
+
+                else if (number == FullTime)
+                {
+                    EmpHrs = 8;
+                }
+
+                else
+                {
+                     EmpHrs = 0;
+                }
+
+                int EmpWages = EmpHrs * EmpPerHour;
+                TotalSalary = (EmpWages + TotalSalary);
+
+            }
+            Console.WriteLine("Total Salary In A Month = " + TotalSalary);
+        }
+
+    }
+}
