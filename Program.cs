48806c0951bff86f02e21dbbc6b0ea952d5bf3cf
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-25 22:25:55 +0530

    adding welcome file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Emp_Report_Using_Oops
+{
+    class program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee report using loops");
+            //EmplyAbsenty.CheckingAbsenty();
+            //PerDayWage.CalculatingWages();
+            //EmpWageWithPartTime.CalculatingWages();
+            //*EmpChecking.UtilizingSwitchCase();
+            //MonthlySalary.CalculatingMonthlyWage();
+            //ConditionalWages.CalculatingWages();
+        }
+    }
+}
