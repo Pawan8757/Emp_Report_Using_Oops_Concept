76b86ce6d9822c8c038c2b29ba21c8ed0beefacd
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-04 23:56:46 +0530

    ability to manage employee for multiple companies

---
+++ Prblm_10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Report_Using_Oops
+{
+    public class Prblm_10
+    {
+        private string company;
+        public int emprateperhr;
+        public int NumOfWorkingPerDays;
+        public int MaxHourPerMonth;
+        public int TotalEmpWage;
+
+
+        public Prblm_10(string company, int emprateperhr, int NumOfWorkingDays, int MaxHourPerMonth)
+        {
+            this.company = company;
+            this.emprateperhr = emprateperhr;
+            this.NumOfWorkingPerDays = NumOfWorkingPerDays;
+            this.MaxHourPerMonth = MaxHourPerMonth;
+
+        }
+
+
+        public void setTotalEmpWage(int TotalEmpwage)
+        {
+            this.TotalEmpWage = TotalEmpWage;
+        }
+
+
+
+
+    }
+}
